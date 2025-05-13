288040f4b67a45734882f0e38ff0074ef774655f
Daniel Jones <djones@danielreddjones.com>
2025-05-12 19:20:50 -0500

    Added comments and a ebook interpreter module

---
+++ src/ebook.rs
@@ -0,0 +1,5 @@
+/*
+ *@author: Daniel Jones
+ *@last modified: May 12 2025
+ *@description: interprets and reads the html in a epub file
+ */
--- src/screen.rs
+++ src/screen.rs
@@ -7,17 +7,21 @@
 extern crate cursive;
 
 pub mod screen {
-use cursive::views::TextView;
-use cursive::Cursive;
+    
+    //Module declarations
+    use cursive::views::TextView;
+    use cursive::Cursive;
 
     pub fn main_screen() {
         let mut siv = cursive::default();
         siv.add_global_callback('q', Cursive::quit);
+        
 
+        //this is the layer in which the ebook text will appear
         siv.add_layer(TextView::new(
-"Hello world! \n\
-Press q to close the application!",
-                ));
+            "Hello world! \n\
+            Press q to close the application!",
+        ));
         siv.run();
     }
 }
